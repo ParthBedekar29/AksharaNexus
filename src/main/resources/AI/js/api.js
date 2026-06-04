88a34abe050ec5a0ae63a5cce53b8fced13039fa
ParthBedekar <parthbedekar62@gmail.com>
2026-06-04 15:09:07 +0530

    Deployment

--- src/main/resources/AI/js/api.js
+++ src/main/resources/AI/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = 'aksharanexus-production.up.railway.app';
 
 const api = {
     getToken: () => localStorage.getItem('akshara_token'),

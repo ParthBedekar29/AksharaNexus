d4ad474f7725f7dc73782ca76eb51fbf9a267b1c
ParthBedekar <parthbedekar62@gmail.com>
2026-06-04 15:23:53 +0530

    Deployment

--- src/main/java/com/example/nexusa/AI/Controller/OracleController.java
+++ src/main/java/com/example/nexusa/AI/Controller/OracleController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/oracle")
+@RequestMapping("/oracle")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class OracleController {
